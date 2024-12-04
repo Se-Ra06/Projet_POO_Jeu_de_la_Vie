84474a0cddc9e52d746e1782f1675f233c9a5df3
wissoLavaca <wissemrecham@gmail.com>
2024-12-04 22:20:36 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include "ModeConsole.h"
+#include "ModeGraphique.h"
+#include "FileService.h"
+#include "Jeu.h"
+#include <direct.h>
+
+
+void afficherMenu() {
+    std::cout << "=============================\n";
+    std::cout << "  Jeu de la Vie - Menu\n";
+    std::cout << "=============================\n";
+    std::cout << "1. Mode Console\n";
+    std::cout << "2. Mode Graphique\n";
+    std::cout << "3. Quitter\n";
+    std::cout << "Choisissez une option : ";
+}
+
+int main() {
+    int choix;
+    do {
+        afficherMenu();
+        std::cin >> choix;
+
+        switch (choix) {
+        case 1: { //Activation du Mode Console
+            std::string inputFile = "FichierInitial.txt";
+            std::string outputDir = inputFile + "_out.txt";
+
+            try {
+                std::cout << "Chargement de la grille depuis : " << inputFile << "\n";
+                Grille grille = FileService::ChargerDuFichier(inputFile);
+
+                int iterations;
+                std::cout << "Entrez le nombre d'iterations : ";
+                std::cin >> iterations;
+
+                
+                Jeu jeu(grille, iterations);
+
+                
+                if (_mkdir(outputDir.c_str()) != 0 && errno != EEXIST) {
+                    throw std::runtime_error("Erreur lors de la creation du repertoire : " + outputDir);
+                } 
+                
+
+                jeu.Simulation(outputDir);
+
+                std::cout << "Simulation console terminee avec succes.\n";
+                std::cout << "Les resultats sont enregistres dans : " << outputDir << "\n";
+            }
+            catch (const std::exception& e) {
+                std::cerr << "Erreur : " << e.what() << "\n";
+            }
+            break;
+        }
+
+        case 2: { //Activation du Mode Graphique
+            std::string inputFile = "FichierInitial.txt";
+
+            try {
+                using GrilleGraph = std::vector<std::vector<bool>>;
+
+                GrilleGraph grille;
+                GrilleGraph nextGrille = grille;
+
+                ModeGraphique modeGraphique;
+                modeGraphique.run();
+                modeGraphique.mettreAJourGrille(grille, nextGrille);
+            }
+            catch (const std::exception& e) {
+                std::cerr << "Erreur : " << e.what() << "\n";
+            }
+            break;
+        }
+
+        case 3:
+            std::cout << "Merci d'avoir utilise le Jeu de la Vie. A bientot !\n";
+            break;
+
+        default:
+            std::cerr << "Option invalide. Veuillez reessayer.\n";
+            break;
+        }
+    } while (choix != 3);
+
+    return 0;
+}
