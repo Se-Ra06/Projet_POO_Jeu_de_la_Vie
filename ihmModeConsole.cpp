f8d506f446b862be46663ebfeb89f5ef22187682
wissoLavaca <wissemrecham@gmail.com>
2024-12-04 22:19:49 +0100

    Update ihmModeConsole.cpp

--- ihmModeConsole.cpp
+++ ihmModeConsole.cpp
@@ -1,31 +1,14 @@
-
-#include<iostream>
-#include<string>
-#include "Fichier.h"
 #include "ModeConsole.h"
-#include"Grille.h"
+#include "FileService.h"
+#include <stdexcept>
+#include <string>
 
-using namespace std;
 
-ModeConsole::ModeConsole(const string& inputFile, const string& outputDir) : inputFile(inputFile), outputDir(outputDir) {}
+ModeConsole::ModeConsole(const std::string& inputFile, const std::string& outputDir)
+    : inputFile(inputFile), outputDir(outputDir) {}
 
 void ModeConsole::run() {
-
-    cout<<"making la grille :"<<inputFile<<endl;
-    Fichier fichier(inputFile);
-    Grille grille = fichier.LireFichier();
-
-    int iterations;
-    cout<<"gimme the number of les iterations:";
-    cin>>iterations;
-
-    Jeu jeu(iterations);
-    jeu.Simulation(grille);
-
-    string outputFile = outputDir + "/output.txt";
-    cout<< "saving la grille after simulation :"<<outputFile<<endl;
-    Fichier fichierSortie(outputFile);
-    fichierSortie.EcrireFichier(grille);
-
-    cout<< "we r done"<<endl;
+    Grille grille = FileService::ChargerDuFichier(inputFile);
+    Jeu jeu(grille.getLargeur(), grille.getLongueur(), 10); 
+    jeu.Simulation(outputDir);
 }
