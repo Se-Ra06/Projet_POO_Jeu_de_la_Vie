903f3ee13ffeb7fb65e879295bf8b6662879f59a
Selma <selma062005@gmail.com>
2024-12-04 22:30:51 +0100

    Ajout de la classe Jeu
    ça marche!!!

---
+++ srvJeu.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <direct.h>
+#include "Jeu.h"
+#include "FileService.h" 
+
+
+
+Jeu::Jeu(int largeur, int longueur, int iterations)
+    : grille(largeur, longueur), iterations(iterations) {}
+
+Jeu::Jeu(const Grille& grille, int iterations)
+    : grille(grille), iterations(iterations) {}
+
+void Jeu::Simulation(const std::string& outputDir) {
+    
+    if (_mkdir(outputDir.c_str()) != 0 && errno != EEXIST) {
+        throw std::runtime_error("Erreur lors de la création du repertoire : " + outputDir);
+    } 
+
+    std::cout << "Debut de la simulation pour " << iterations << " iterations" << std::endl;
+
+   
+    FileService::SauvegarderIteration(grille, outputDir, 0);
+
+    
+    for (int i = 1; i <= iterations; ++i) {
+        std::cout << "Iteration " << i << "/" << iterations << std::endl;
+
+       
+        MiseAJourGrille();
+
+        
+        FileService::SauvegarderIteration(grille, outputDir, i);
+    }
+
+    std::cout << "Simulation terminee" << std::endl;
+}
+
+void Jeu::MiseAJourGrille() {
+    std::vector<std::vector<bool>> prochainsEtats(grille.getLongueur(), std::vector<bool>(grille.getLargeur()));
+
+    
+    for (int x = 0; x < grille.getLongueur(); ++x) {
+        for (int y = 0; y < grille.getLargeur(); ++y) {
+            int voisinsVivants = grille.NombreVoisinsVivants(x, y);
+            prochainsEtats[x][y] = grille.getEtat(x, y)
+                ? (voisinsVivants == 2 || voisinsVivants == 3)
+                : (voisinsVivants == 3);
+        }
+    }
+
+    
+    for (int x = 0; x < grille.getLongueur(); ++x) {
+        for (int y = 0; y < grille.getLargeur(); ++y) {
+            grille.SetCelluleEtat(x, y, prochainsEtats[x][y]);
+        }
+    }
+
+   
+    std::cout << "Etat actuel de la grille :\n";
+    for (int x = 0; x < grille.getLongueur(); ++x) {
+        for (int y = 0; y < grille.getLargeur(); ++y) {
+            std::cout << (grille.getEtat(x, y) ? '1' : '0') << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << std::string(30, '-') << "\n"; 
+}
---
+++ srvJeu.h
@@ -0,0 +1,19 @@
+#ifndef JEU_H
+#define JEU_H
+
+#include "Grille.h"
+#include <string>
+
+class Jeu {
+private:
+    Grille grille;
+    int iterations;
+
+public:
+    Jeu(int largeur, int longueur, int iterations);
+    Jeu(const Grille& grille, int iterations);
+    void Simulation(const std::string& outputDir);
+    void MiseAJourGrille();
+};
+
+#endif 
