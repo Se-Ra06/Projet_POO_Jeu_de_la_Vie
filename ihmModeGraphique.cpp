3596f0b37a1db7695dcae5a5e93b12104331d00a
Selma <selma062005@gmail.com>
2024-12-07 19:02:14 +0100

    Amélioration du mode graphique

--- ihmModeGraphique.cpp
+++ ihmModeGraphique.cpp
@@ -5,41 +5,34 @@
 #include <vector> 
 
 void ModeGraphique::run() {
-
     sf::RenderWindow window(sf::VideoMode(GRILLE_LARGEUR * TAILLE_CELLULE, GRILLE_LONGUEUR * TAILLE_CELLULE), "Jeu de la Vie");
     window.setFramerateLimit(60);
-
-    // initialisation de la grille
 
     GrilleGraph grille(GRILLE_LONGUEUR, std::vector<bool>(GRILLE_LARGEUR, false));
     GrilleGraph nextGrille = grille;
 
-    // Variables de simulation
     sf::Clock clock;
     bool isRunning = false;
     bool manualMode = true;
 
-
+    float cellWidth = TAILLE_CELLULE;
+    float cellHeight = TAILLE_CELLULE;
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-
-            // Interaction avec la souris
             if (event.type == sf::Event::MouseButtonPressed) {
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    int x = event.mouseButton.x / TAILLE_CELLULE;
-                    int y = event.mouseButton.y / TAILLE_CELLULE;
+                    int x = event.mouseButton.x / cellWidth;
+                    int y = event.mouseButton.y / cellHeight;
                     if (x >= 0 && x < GRILLE_LARGEUR && y >= 0 && y < GRILLE_LONGUEUR) {
                         if (manualMode) {
-                            // Changer l'état des cellules individuellement avec le clic gauche de la souris
                             grille[y][x] = !grille[y][x];
                         }
                     }
                 }
             }
 
-            // Interaction avec l'interface à travers les touches du clavier
             if (event.type == sf::Event::Closed) window.close();
             if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Space) {
@@ -50,22 +43,28 @@ void ModeGraphique::run() {
                 }
             }
 
+            if (event.type == sf::Event::Resized) {
+                float newWidth = event.size.width;
+                float newHeight = event.size.height;
+
+                cellWidth = newWidth / GRILLE_LARGEUR;
+                cellHeight = newHeight / GRILLE_LONGUEUR;
+
+                window.setView(sf::View(sf::FloatRect(0, 0, newWidth, newHeight)));
+            }
         }
 
-        // Appel de la fonction pour mettre à jour la grille
         if (isRunning && clock.getElapsedTime().asMilliseconds() > 100) {
             mettreAJourGrille(grille, nextGrille);
             clock.restart();
         }
 
-        // Dessiner l'interface
         window.clear(sf::Color(10, 10, 30));
 
-        // Dessiner les cellules
         for (int y = 0; y < GRILLE_LONGUEUR; ++y) {
             for (int x = 0; x < GRILLE_LARGEUR; ++x) {
-                sf::RectangleShape cell(sf::Vector2f(TAILLE_CELLULE - 2, TAILLE_CELLULE - 2));
-                cell.setPosition(x * TAILLE_CELLULE, y * TAILLE_CELLULE);
+                sf::RectangleShape cell(sf::Vector2f(cellWidth - 2, cellHeight - 2));
+                cell.setPosition(x * cellWidth, y * cellHeight);
                 if (grille[y][x]) {
                     cell.setFillColor(sf::Color(128, 0, 128));
                 }
@@ -76,11 +75,10 @@ void ModeGraphique::run() {
             }
         }
 
-
         window.display();
     }
-
 }
+
 
 // Méthode pour mettre à jour la grille
 void ModeGraphique::mettreAJourGrille(GrilleGraph& grille, GrilleGraph& nextGrille) {
@@ -96,6 +94,7 @@ void ModeGraphique::mettreAJourGrille(GrilleGraph& grille, GrilleGraph& nextGril
                     // Calculer les coordonnées des voisins sur une grille torique
                     int ny = (y + dy + GRILLE_LONGUEUR) % GRILLE_LONGUEUR;
                     int nx = (x + dx + GRILLE_LARGEUR) % GRILLE_LARGEUR;
+
 
                     voisinsvivants += grille[ny][nx]; // Ajouter l'état de la cellule voisine
                 }
@@ -114,4 +113,3 @@ void ModeGraphique::mettreAJourGrille(GrilleGraph& grille, GrilleGraph& nextGril
     // Mise à jour de la grille avec la nouvelle génération
     grille = nextGrille;
 }
-
